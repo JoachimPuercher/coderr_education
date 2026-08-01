ad63cf54bfe9a274b18dc4c73bd2f7c3961dd03d
JoachimPuercher <joachim.puercher@gmail.com>
2026-08-01 16:45:03 +0200

    rename(url): new name profil

--- profile_app/api/urls.py
+++ profile_app/api/urls.py
@@ -3,7 +3,7 @@ from .views import ProfileView, BusinessProfileView, CustomerProfileView
 
 
 urlpatterns = [
-    path('profile/<int:pk>/', ProfileView.as_view(), name='registration'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profil'),
     path('profile/business/', BusinessProfileView.as_view(), name='business_profile_list'),
     path('profile/customer/', CustomerProfileView.as_view(), name='customer_profile_list')
 ]
