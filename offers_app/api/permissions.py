a40c5738c97f997d9c6f1dbefca07203e3c18e93
JoachimPuercher <joachim.puercher@gmail.com>
2026-08-07 14:06:33 +0200

    feat(offers): add search filter and move read access to the view
    Adds SearchFilter over title and description. The SAFE_METHODS branch
    moves from IsBussinesUser into get_permissions.

--- offers_app/api/permissions.py
+++ offers_app/api/permissions.py
@@ -7,11 +7,10 @@ class IsBussinesUser(BasePermission):
 
     def has_permission(self, request, view):
 
-        if request.method in SAFE_METHODS:
-            return True
-
+        self.message = "You are not allowed, only business user can create offers."
+        
         if request.user.userprofile.type == 'business_user':
-            self.message = "You are not allowed, only business user can create offers."
+            
             return True
 
         return False
--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
-from rest_framework.permissions import AllowAny, SAFE_METHODS, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import AllowAny, SAFE_METHODS, IsAuthenticated
+from rest_framework.filters import SearchFilter
 from .serializers import OfferMainSerializer
 from .permissions import IsBussinesUser
 from offers_app.models import Offer
@@ -8,11 +9,15 @@ from .filters import OfferFilter
 
 class OfferMainView(generics.ListCreateAPIView):
     serializer_class = OfferMainSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsBussinesUser]
     queryset = Offer.objects.all()
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = OfferFilter
-
+    search_fields = ["title", "description"]
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-        
+
+    def get_permissions(self):
+                if self.request.method in SAFE_METHODS:
+                    return [AllowAny()]
+                else:
+                    return [IsAuthenticated(), IsBussinesUser()]
